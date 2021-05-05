03697674796543341ed4e54ab08e4863c766fa69
Aleh13 <q4wk13@gmail.com>
2021-05-05 13:15:44 +0300

    Создали эндпоинт destroy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,6 +16,7 @@ module Api
 
             def create
               @user = User.new(user_params)
+
               if @user.save
             	  render json: @user.to_json
               else
@@ -25,14 +26,22 @@ module Api
 
 
             def update
+              @user = User.find(params[:id])
+
               if @user.update(user_params)
                 render json: @user
               else
                 render json: { errord 'нихуя не работает'}
               end
             end
-              	
 
+
+            def destroy
+              @user = User.find(params[:id])
+              @article.destroy
+
+              render json: @user
+            end	
 
 
             private
