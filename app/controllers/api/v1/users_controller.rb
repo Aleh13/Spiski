e5cfd04a385583ae56dfa8119be63ab4273c3fad
Aleh13 <q4wk13@gmail.com>
2021-05-05 12:53:55 +0300

    Создали эндпоинт update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,13 +15,23 @@ module Api
 
 
             def create
-            	@user = User.new(user_params)
-            	if @user.save
-            		render json: @user.to_json
-                else
-                	render json: { errord 'нихуя не работает'}
-                end
+              @user = User.new(user_params)
+              if @user.save
+            	  render json: @user.to_json
+              else
+                render json: { errord 'нихуя не работает'}
+              end
             end
+
+
+            def update
+              if @user.update(user_params)
+                render json: @user
+              else
+                render json: { errord 'нихуя не работает'}
+              end
+            end
+              	
 
 
 
