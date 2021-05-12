c1da9ea760f962ef9301ed1c307de8c9cbc80c3b
Aleh13 <q4wk13@gmail.com>
2021-05-12 17:00:37 +0300

    Добавлены валидации

--- README.md
+++ README.md
@@ -60,7 +60,7 @@ jwt
 
 bcrypt
 
-bcrypt
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true
+  validates :password, length: { minimum: 6 }
+  validates_format_of :email, :with => /@/, uniqueness: true
+
 end
