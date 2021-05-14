2b18aa75af92f670bccdbcdba22de9d477e9b41f
Aleh13 <q4wk13@gmail.com>
2021-05-14 17:06:55 +0300

    Оформление user.rb с помощью rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-	has_secure_password
 
-	validates :name, presence: true
-	validates :password, length: { minimum: 6 }
-	validates_format_of :email, with: /@/
-	validates :email, uniqueness: true
+  has_secure_password
+
+  validates :name, presence: true
+  validates :password, length: { minimum: 6 }
+  validates_format_of :email, :with => /@/
+  validates :email, uniqueness: true
+  
 end
